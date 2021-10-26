3077864f7f90a70b8565e358de44f5bee0981297
F-O-N-S-E-C-A <tiago_fonseca@iscte-iul.pt>
2021-10-26 19:24:30 +0100

    Commit

--- qc_multiple_svm.py
+++ qc_multiple_svm.py
@@ -21,8 +21,8 @@ def main(validation_file, train_file, operation):
     corpus_validation = pd.read_csv(validation_file, sep='\t', error_bad_lines=False, header=None, names=["Label", "Question", "Answer"])
     print(corpus)
 
-    questionSVM, question_prediction = question_svm(corpus, corpus_validation)
-    answerSVM, answer_prediction = answer_svm(corpus, corpus_validation)
+    questionSVM, question_prediction = build_svm(corpus, corpus_validation, data_part='Question')
+    answerSVM, answer_prediction = build_svm(corpus, corpus_validation, data_part='Answer')
 
     #print(question_prediction)
     #print(answer_prediction)
@@ -30,25 +30,33 @@ def main(validation_file, train_file, operation):
     encoder = LabelEncoder()
     labels = encoder.fit_transform(corpus_validation['Label'])
 
-    accuracy_question = accuracy_score(prob_vector_to_guess(question_prediction), labels)
-    accuracy_answer = accuracy_score(prob_vector_to_guess(answer_prediction), labels)
-
-    print('Accuracy Question SVM: ', accuracy_question, ' Accuracy Answer SVM: ', accuracy_answer)
-
     prediction = []
-    for i in range(0, len(answer_prediction)):
-        # max - Accuracy:  0.878
-        # weighted_sum - Accuracy: 0.864
-        if operation == 'max':
+
+    # max - Accuracy:  0.878
+    # weighted_sum - Accuracy: 0.864
+    # joint_probability - Accuracy: 0.864
+    if operation == 'max':
+        for i in range(0, len(answer_prediction)):
             if max(question_prediction[i]) > max(answer_prediction[i]):
                 prediction.append(np.argmax(question_prediction[i]))
             else:
                 prediction.append(np.argmax(answer_prediction[i]))
-        elif operation == 'weighted_sum':
-            # avr
+    elif operation == 'weighted_sum':
+
+        accuracy_question = accuracy_score(prob_vector_to_guess(question_prediction), labels)
+        accuracy_answer = accuracy_score(prob_vector_to_guess(answer_prediction), labels)
+        print('Accuracy Question SVM: ', accuracy_question, ' Accuracy Answer SVM: ', accuracy_answer)
+
+        for i in range(0, len(answer_prediction)):
             vec = []
             for j in range(0, len(question_prediction[i])):
                 vec.append(question_prediction[i][j] * accuracy_question + answer_prediction[i][j] * accuracy_answer)
+            prediction.append(vec.index(max(vec)))
+    elif operation == 'joint_probability':
+        for i in range(0, len(answer_prediction)):
+            vec = []
+            for j in range(0, len(question_prediction[i])):
+                vec.append(question_prediction[i][j] * answer_prediction[i][j])
             prediction.append(vec.index(max(vec)))
 
     print('Accuracy: ', accuracy_score(prediction, labels))
@@ -60,39 +68,12 @@ def prob_vector_to_guess(list):
         res.append(np.argmax(l))
     return res
 
+def build_svm(corpus, corpus_validation, data_part):
+    tokenization(corpus, data_part)
+    tokenization(corpus_validation, data_part)
 
-def question_svm(corpus, corpus_validation):
-    tokenization(corpus, 'Question')
-    tokenization(corpus_validation, 'Question')
-
-    lemmatization(corpus, 'Question')
-    lemmatization(corpus_validation, 'Question')
-
-    # print(corpus)
-    # Train_X, Test_X, train_Y, test_Y = model_selection.train_test_split(corpus['lemma'], corpus[0], test_size=0.3)
-
-    encoder = LabelEncoder()
-    train_Y = encoder.fit_transform(corpus['Label'])
-    test_Y = encoder.fit_transform(corpus_validation['Label'])
-
-    Tfidf_vect = TfidfVectorizer(max_features=5000)
-    Tfidf_vect.fit(corpus['lemma_Question'])
-    train_X = Tfidf_vect.transform(corpus['lemma_Question'])
-    test_X = Tfidf_vect.transform(corpus_validation['lemma_Question'])
-
-    # print(Tfidf_vect.vocabulary_)
-    # print(train_X_vector)
-    svm = support_vector_machine(train_X, test_X, train_Y, test_Y)
-    prediction = svm.predict_proba(test_X)
-    return svm, prediction
-
-def answer_svm(corpus, corpus_validation):
-    #corpus['Answer'] = [str(entry) for entry in corpus['Answer']]
-    tokenization(corpus, 'Answer')
-    tokenization(corpus_validation, 'Answer')
-
-    lemmatization(corpus, 'Answer')
-    lemmatization(corpus_validation, 'Answer')
+    lemmatization(corpus, data_part)
+    lemmatization(corpus_validation, data_part)
 
     # print(corpus)
     # Train_X, Test_X, train_Y, test_Y = model_selection.train_test_split(corpus['lemma'], corpus[0], test_size=0.3)
@@ -102,9 +83,10 @@ def answer_svm(corpus, corpus_validation):
     test_Y = encoder.fit_transform(corpus_validation['Label'])
 
     Tfidf_vect = TfidfVectorizer(max_features=5000)
-    Tfidf_vect.fit(corpus['lemma_Answer'])
-    train_X = Tfidf_vect.transform(corpus['lemma_Answer'])
-    test_X = Tfidf_vect.transform(corpus_validation['lemma_Answer'])
+    lemma = 'lemma_' + data_part
+    Tfidf_vect.fit(corpus[lemma])
+    train_X = Tfidf_vect.transform(corpus[lemma])
+    test_X = Tfidf_vect.transform(corpus_validation[lemma])
 
     # print(Tfidf_vect.vocabulary_)
     # print(train_X_vector)
@@ -167,7 +149,7 @@ def man():
 if __name__ == '__main__':
     if len(sys.argv) == 5:
         if sys.argv[1] == '-test' and sys.argv[3] == '-train':
-            main(validation_file=sys.argv[2], train_file=sys.argv[4], operation='weighted_sum')
+            main(validation_file=sys.argv[2], train_file=sys.argv[4], operation='joint_probability')
         else:
             man()
     else:
