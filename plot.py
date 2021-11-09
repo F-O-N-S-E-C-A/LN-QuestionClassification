40fb5f13ceaa344f691aa60d70413b916c6507ff
swNotJoao <jclopes99@gmail.com>
2021-11-09 16:12:38 +0000

    fix plot once again

--- plot.py
+++ plot.py
@@ -3,11 +3,15 @@ from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
 import numpy as np
 import seaborn as sns
+from mpl_toolkits.mplot3d import Axes3D
 
 def PLOT(corpus, train_X, train_Y):
-    pca = PCA(n_components = 9).fit(train_X.toarray())
+    pca = PCA(n_components = 4).fit(train_X.toarray())
     X_pca = pca.transform(train_X.toarray())
 
-    labels = [corpus[0][i] for i in train_Y]
+    labels = [corpus[0][i] for i in corpus[0]]
+
+    print(labels)
+
     sns.scatterplot(X_pca[:,0], X_pca[:, 1],  hue=labels, legend='full',palette="Set2")
     plt.show()
